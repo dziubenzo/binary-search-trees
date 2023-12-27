2fc56ce71c013a51bf1d0ca590e9dfe638e7620e
dziubenzo <dziubenzo@gmail.com>
2023-12-27 18:28:37 +0100

    Remove duplicates from and sort input array

--- main.js
+++ main.js
@@ -22,10 +22,20 @@ class Node {
 
 class Tree {
   constructor(array) {
+    // Accept only arrays
     if (!(array instanceof Array)) {
       throw new Error('Error: Array required.');
     }
+    // Keep original array
     this.originalArray = array;
+    // Remove duplicates from and sort the original array;
+    let noDuplicates = [];
+    array.forEach((value) => {
+      if (!noDuplicates.includes(value)) {
+        noDuplicates.push(value);
+      }
+    });
+    this.sortedArray = noDuplicates.sort((a, b) => a - b);
     this.root = this.buildTree();
   }
 
@@ -40,7 +50,7 @@ class Tree {
       root.right = createBST(array, mid + 1, end);
       return root;
     };
-    return createBST(this.array, 0, this.array.length - 1);
+    return createBST(this.sortedArray, 0, this.sortedArray.length - 1);
   }
 }
 
@@ -48,5 +58,6 @@ const array0 = [1, 2, 3, 4];
 const array1 = [0, 1, 2, 3, 4, 5, 6, 7];
 const array2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
-let tree = new Tree(array1);
+let tree = new Tree(array2);
 prettyPrint(tree.root);
+console.log(tree);
